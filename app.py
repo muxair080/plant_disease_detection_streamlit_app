9912c99ae623678576fff4e2b8c2352e7332bec1
Muhammad Uzair <uzair1kk2019@gmail.com>
2023-07-19 14:33:43 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,91 @@
+import streamlit as st
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import base64
+import cv2
+MODEL = tf.keras.models.load_model('./photato_trained_models/1/')
+TOMATO_MODEL = tf.keras.models.load_model('./tomato_trained_models/1')
+PEEPER_MODEL = tf.keras.models.load_model('./pepeer_trained_models/1')
+class_names = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+Tomato_classes = ['Tomato_healthy', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot', 'Tomato_Septoria_leaf_spot',
+ 'Tomato__Tomato_mosaic_virus', 'Tomato_Leaf_Mold', 'Tomato_Bacterial_spot', 'Tomato_Late_blight',
+ 'Tomato_Early_blight', 'Tomato__Tomato_YellowLeaf__Curl_Virus']
+pepper_classes = ['pepper_healthy', 'pepper_bell_bacterial_spot']
+st.set_page_config(
+    layout="wide",
+    page_title='plant disease detection',
+)
+st.title("Plant Disease Detection")
+st.write("This application is detecting disease in three plants photato, tomato and pepper")
+options = ["Select One Plant","Tomato", "Photato", "Pepper"]
+
+    # Create a selectbox for the user to choose one option
+selected_option = st.selectbox("Select Plant:", options)
+
+# st.write("You selected:", selected_option)
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+def read_file_as_image(data)->np.array:
+    image = np.array(data)
+    image = cv2.resize(image, (256,256))
+    return image
+
+async def photato():
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", width=250)
+        image = read_file_as_image(image)
+        image_batch = np.expand_dims(image, axis=0)
+        predictions = MODEL.predict(image_batch)
+        predicted_class = class_names[np.argmax(predictions[0])]
+        confidence = np.max(predictions[0])
+        print("prediction", class_names[np.argmax(predictions)])
+        st.write("Predicted Class : ", predicted_class, " Confidence Level : ", confidence)
+
+
+async def tomato():
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+
+        #
+        st.image(image, caption="Uploaded Image", width=250)
+        image = read_file_as_image(image)
+        image_batch = np.expand_dims(image, axis=0)
+        predictions = TOMATO_MODEL.predict(image_batch)
+        predicted_class = Tomato_classes[np.argmax(predictions[0])]
+        confidence = np.max(predictions[0])
+        print("prediction", Tomato_classes[np.argmax(predictions)])
+        st.write("Predicted Class : ", predicted_class, " Confidence Level : ", confidence)
+
+
+async def peeper():
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+
+        #
+        st.image(image, caption="Uploaded Image", width=250)
+        image = read_file_as_image(image)
+
+        image_batch = np.expand_dims(image, axis=0)
+        predictions = PEEPER_MODEL.predict(image_batch)
+        predicted_class = pepper_classes[np.argmax(predictions[0])]
+        confidence = np.max(predictions[0])
+        print("prediction", pepper_classes[np.argmax(predictions)])
+        st.write("Predicted Class : ", predicted_class, "         Confidence Level : ", confidence)
+
+import asyncio
+
+if __name__ == "__main__":
+    # if st.button('Predict'):
+    
+        if selected_option == 'Photato':
+            asyncio.run(photato())
+        elif selected_option == 'Tomato':
+            asyncio.run(tomato())
+        elif selected_option == 'pepper':
+                asyncio.run(peeper())
+        else:
+            st.write("not avalible")
